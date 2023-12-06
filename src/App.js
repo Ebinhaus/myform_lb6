06a12d5aaf32a9f6d2705989219127db6c378c9b
Ebinhaus <stepa.r67123456789@gmail.com>
2023-12-06 10:03:02 +0300

    w

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return (
+  <div className="App">
+
+<h2>React - приложение отправки данных </h2>
+<h2>клиента на сервер от Макса</h2>
+    <form action="http://127.0.0.1:5000/find" method="POST" className="form">
+    <fieldset>
+     <p className="item">
+     <label> Ваше имя </label>
+     <input type="text" name="username" id="username" />
+     </p>
+     <p className="item">
+      <label> Пароль </label>
+      <input type="password" name="password" id="password" />
+     </p>
+     <button type="submit">Послать на сервер</button>
+    </fieldset>
+    </form>
+    <img src={logo} className="App-logo" alt="logo" width="250" height="240" />
+
+  </div>
+ );
 }
 
 export default App;
+// npm install @babel/plugin-proposal-private-property-in-object
